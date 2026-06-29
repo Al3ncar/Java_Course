6631f65ad50ae5d46932edf382bfe4f3695f2aeb
Al3ncar <alencar29htcss@gmail.com>
2026-06-29 01:35:27 -0300

    3.0 Aula Vetor(Array)

---
+++ Aula-03/Main.java
@@ -0,0 +1,22 @@
+public class Main{
+    public static void main(String[] args){
+        // 3.0 Aula Vetor(Array)
+        int[] collectionNumber = {1, 2, 3, 4, 5};
+        int[] arrays = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] arr = new int[10];
+
+
+        String[] names = new String[5];
+
+        names[0] = "Afonso";
+        names[1] = "Bruna";
+        names[2] = "Melissa";
+
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+
+        System.out.println(" ");
+        System.out.println(names.length);
+    }
+}
