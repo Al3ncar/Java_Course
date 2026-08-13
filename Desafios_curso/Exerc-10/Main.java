4a1e6de81945ec1b3b171ca393b8b4e61cbc9d39
Igor Bastos Alencar <alencar29htcss@gmail.com>
2026-08-13 17:16:57 -0300

    Soma Vector

--- Desafios_curso/Exerc-10/Main.java
+++ Desafios_curso/Exerc-10/Main.java
@@ -1,5 +1,3 @@
-import jdk.swing.interop.SwingInterOpUtils;
-
 import java.util.Scanner;
 import java.util.Locale;
 
@@ -10,40 +8,56 @@ public class Main {
 
 
         // 1.0 Exercicio Problema Negativos
-        System.out.println("NEGATIVOS");
-        System.out.println("-----------------------------------------");
-        System.out.print("Quantos numeros voce vai digitar? ");
-        int qtdNegativos = sc.nextInt();
-        int[] valueNeg = new int[qtdNegativos];
-
-
-        int j = 0;
-        while (j < valueNeg.length) {
-            System.out.print("Digite um numero: ");
-            valueNeg[j] = sc.nextInt();
-            j++;
-        }
-
-        System.out.println(" ");
-        System.out.println("NUMEROS NEGATIVOS: ");
-
-        for (int n : valueNeg) {
-            if (n < 0) {
-                System.out.println(n);
-            }
-        }
+//        System.out.println("NEGATIVOS");
+//        System.out.println("-----------------------------------------");
+//        System.out.print("Quantos numeros voce vai digitar? ");
+//        int qtdNegativos = sc.nextInt();
+//        int[] valueNeg = new int[qtdNegativos];
+//
+//
+//        int j = 0;
+//        while (j < valueNeg.length) {
+//            System.out.print("Digite um numero: ");
+//            valueNeg[j] = sc.nextInt();
+//            j++;
+//        }
+//
+//        System.out.println(" ");
+//        System.out.println("NUMEROS NEGATIVOS: ");
+//
+//        for (int n : valueNeg) {
+//            if (n < 0) {
+//                System.out.println(n);
+//            }
+//        }
 
         // 2.0 Soma_Vetor
-        System.out.println("NEGATIVOS");
+        System.out.println("soma_vetor");
         System.out.println("-----------------------------------------");
+
         System.out.print("Quantos numeros voce vai digitar? ");
+
+        double[] vectSoma;
+        double resultVectSoma, resultVectAvg;
+        StringBuilder printResult = new StringBuilder();
+
         int qtdSomaVetor = sc.nextInt();
-        int[] vectSoma = new int[qtdNegativos];
+        vectSoma = new double[qtdSomaVetor];
+        resultVectSoma = 0;
+
 
         for (int i = 0; i < vectSoma.length; i++) {
             System.out.print("Digite um numero: ");
-            valueNeg[j] = sc.nextInt();
-
+            vectSoma[i] = sc.nextInt();
+            resultVectSoma += vectSoma[i];
+            printResult.append(vectSoma[i]).append(" ");
         }
+        System.out.println(" ");
+        System.out.println("VALORES = " + printResult);
+        System.out.printf("SOMA = %.2f %n", resultVectSoma);
+        System.out.printf("MEDIA = %.2f %n", resultVectSoma / vectSoma.length);
+
+        //3.0 alturas
+
     }
 }
