118291617192421e408f10724157a08339d9ef68
Igor Bastos Alencar <alencar29htcss@gmail.com>
2026-09-23 17:32:39 -0300

    Added new login in exec 11

--- Desafios_curso/Exerc-11/Main.java
+++ Desafios_curso/Exerc-11/Main.java
@@ -1,4 +1,35 @@
-package PACKAGE_NAME;
+import entities.Persons;
+
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args){
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Quantas empregados seram registrados: ");
+        int qtdEmployees = sc.nextInt();
+        sc.nextLine();
+
+        Persons[] person = new Persons[qtdEmployees];
+
+
+        for(int i = 0; i < person.length; i++){
+            System.out.println(person[i]);
+            System.out.print("ID: ");
+            int id = sc.nextInt();
+
+            System.out.print("NAME: ");
+            String name = sc.nextLine();
+
+            System.out.print("SALARY: ");
+            int id = sc.nextInt();
+
+            sc.nextLine();
+
+        }
+
+        sc.close();
+    }
 }
--- Desafios_curso/Exerc-11/entities/Persons.java
+++ Desafios_curso/Exerc-11/entities/Persons.java
@@ -1,4 +1,14 @@
 package entities;
 
 public class Persons {
+    private int id;
+    private String name;
+    private double salary;
+
+
+    public Persons(int id, String name, double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
 }
